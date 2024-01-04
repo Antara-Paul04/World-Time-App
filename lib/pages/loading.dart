22775536fdcf2c0b1ad075d7b00c53f9531a5c35
Antara Paul <hiiamantara04@gmail.com>
2024-01-04 22:12:03 +0530

    feat:Display place and local time information in the Home screen. Populate Home screen with location details and corresponding time fetched from the WorldTime API. Integrate loading spinner during data fetching in the _Loading screen using flutter_spinkit

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'package:world_time_app/services/world_time.dart';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -12,6 +12,7 @@ class Loading extends StatefulWidget {
 class _Loading extends State<Loading> {
 
   void setupWorldTime() async{
+    await Future.delayed(Duration(seconds: 2));
     worldTime instance=worldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     // print (instance.time);
@@ -30,11 +31,15 @@ class _Loading extends State<Loading> {
     setupWorldTime();
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Text("Loading screen"),
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.blue,
+          size: 50.0,
+        ),
+      ),
     );
   }
 }
